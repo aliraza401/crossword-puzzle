602e1bc39f74c1ccb53b27d8da1a76237036af5d
Ghost <alirazamunir401@gmail.com>
2023-05-08 15:55:21 +0500

    add game end event

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,22 +1,36 @@
-import React from "react";
-import Crossword from "@jaredreisinger/react-crossword";
+import React, { useRef } from "react";
+import Crossword, {
+  CrosswordProviderImperative,
+} from "@jaredreisinger/react-crossword";
 
 import { GameProps } from "./Game.interface";
 import {} from "./Game.styled";
 
 import { useCrosswordData } from "../../context/crosswordDataContext";
-import { Spin } from "antd";
+import { Spin, message } from "antd";
 import { Header } from "../../components/Header/Header";
 
 export const Game: React.FC<GameProps> = () => {
   const [crosswordData] = useCrosswordData();
+  const gameRef = useRef<any>();
+
+  const onComplete = () => {
+    message.success("Puzzle Completed Success");
+    gameRef.current.reset();
+  };
 
   if (!crosswordData) return <Spin />;
 
   return (
     <>
       <Header name="Game" />
-      <Crossword data={crosswordData} />
+      <Crossword
+        data={crosswordData}
+        useStorage={false}
+        ref={gameRef}
+        onCorrect={(e) => console.log(e)}
+        onCrosswordCorrect={onComplete}
+      />
     </>
   );
 };
